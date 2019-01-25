cb95ea4681012d675ac6a6341caf7e15650e5e2c
Rainer M. Krug <Rainer@krugs.de>
2019-01-25 11:12:17 +0100

    addAttributes.R: Remove linefeeds to avoid errors

--- R/addAttributes.R
+++ R/addAttributes.R
@@ -15,9 +15,11 @@ addAttributes <- function(x, nm, puml){
     )
     for (i in 1:length(attributes(x))) {
       if (names(attributes(x))[i] != "class") {
+        attrval <- paste0(attributes(x)[[i]], collapse = " ")
+        attrval <- gsub("[\r\n]", "", attrval)
         puml$code <- paste0(
           puml$code, " \n ",
-          "\"", nm, "\" : .", names(attributes(x))[i], " = ", paste0(attributes(x)[[i]], collapse = " "),
+          "\"", nm, "\" : .", names(attributes(x))[i], " = ", attrval,
           " \n "
         )
       }
