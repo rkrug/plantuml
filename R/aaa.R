b91f732636dc3ee2e070a40b96ee8cc72626eae1
Rainer M. Krug <Rainer@krugs.de>
2020-07-03 15:04:07 +0200

    Next try with `backports`

--- R/aaa.R
+++ R/aaa.R
@@ -1,13 +1,11 @@
 #' @import backports
-#' @importFrom utils getFromNamespace
-
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, c("R_user_dir"), force = TRUE)
 }
 .onAttach <- function(libname, pkgname) {
 
   setPlantumlOption(
-    jar_path = file.path( tools::R_user_dir(package = "plantuml", "cache"), "jar"),
+    jar_path = file.path( R_user_dir(package = "plantuml", "cache"), "jar"),
     jar_name = "plantuml.jar",
     java_bin = Sys.which("java"),
     java_opt = "-Djava.awt.headless=true -splash:no"
