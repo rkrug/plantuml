7b68782796440213b071548f11182373c23b5bfe
Rainer M. Krug <Rainer@krugs.de>
2022-01-24 19:25:47 +0100

    First try plantuml encode

---
+++ R/encodePlantUMLServer..R
@@ -0,0 +1,119 @@
+## Functions based on https://plantuml.com/code-javascript-synchronous and https://plantuml.com/code-php.
+## Also see https://forum.plantuml.net/15331/example-on-how-to-encode-for-plantuml-server
+
+encode64 <- function(
+  data
+){
+  r  <- ""
+  #
+  for (i in seq(from = 0, to = length(data), by = 3)) {
+    if (i+2 == length(data)) {
+      r <- paste0(
+        r,
+        append3bytes(
+          intToUtf8(data[i+1]),
+          intToUtf8(data[i+2]),
+          0
+        )
+      )
+    } else if (i+1 == length(data)) {
+      r <- paste0(
+        r,
+        append3bytes(
+          intToUtf8(data[i+1]),
+          0,
+          0
+        )
+      )
+    } else {
+      r <- paste0(
+        r,
+        append3bytes(
+          intToUtf8(data[i+1]),
+          intToUtf8(data[i+2]),
+          intToUtf8(data[i+3])
+        )
+      )
+    }
+  }
+  return(r);
+}
+
+append3bytes <- function(
+  b1,
+  b2,
+  b3
+){
+  c1 <-  bitwShiftR( a = b1, n = 2)
+  c2 <-  bitwOr(
+    bitwShiftL( a = bitwAnd(b1, 0x3), n = 4),
+    bitwShiftR( a = b2, n = 4)
+  )
+  c3 <-  bitwOr(
+    bitwShiftL( a = bitwAnd(b2, 0xF), n = 2),
+    bitwShiftR( a = b3, n = 6)
+  )
+  c4 <-  bitwAnd(b3, 0x3F)
+  #
+  txt  <-  ""
+  txt <- paste0(txt, encode6bit(bitwAnd(c1, 0x3F)))
+  txt <- paste0(txt, encode6bit(bitwAnd(c2, 0x3F)))
+  txt <- paste0(txt, encode6bit(bitwAnd(c3, 0x3F)))
+  txt <- paste0(txt, encode6bit(bitwAnd(c4, 0x3F)))
+  #
+  return(txt)
+}
+
+encode6bit <- function(
+  int
+){
+  if (int < 10) {
+    char <- intToUtf8(48 + int)
+  }
+  int  <- int - 10
+  #
+  if (int < 26) {
+    char <- intToUtf8(65 + int)
+  }
+  int  <- int - 26
+  #
+  if (int < 26) {
+    char <- intToUtf8(97 + int)
+  }
+  int  <- int - 26
+  #
+  if (int == 0) {
+    char <- "-"
+  }
+  #
+  if (int == 1) {
+    char <- "_"
+  }
+  char <- "?"
+  #
+  return(char)
+}
+
+#' Generate PlantUML Server URL
+#'
+#' @param plantuml The plantuml code
+#' @param server_url The base url of the server
+#'
+#' @return complete url
+#' @export
+#'
+#' @examples
+#'
+plantuml_URL <- function(
+  plantuml = "@startuml\nBob -> Alice : hello\n@enduml",
+  server_url = "http://www.plantuml.com/plantuml/png/"
+){
+  plantuml <- enc2utf8(plantuml)
+  comp <- brotli::brotli_compress(charToRaw(plantuml))
+  # comp <- memCompress(charToRaw('plantuml'), "gzip")
+
+  enc <- encode64(comp)
+  url <- paste0(server_url, "/", enc)
+
+  return(url)
+}
